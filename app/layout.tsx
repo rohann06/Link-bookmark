dda359e6e17c69b5f4d32e47cb9bf48280ac737c
Rohan <rohanahire651@gmail.com>
2023-08-06 12:16:16 +0530

    initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html className='mx-4 md:mx-20 my-5' lang="en">
       <body className={inter.className}>{children}</body>
     </html>
   )
