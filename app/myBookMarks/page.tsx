b437cf185b3ecd8a1cf37c2488fcc7ca4ec58a54
Rohan <rohanahire651@gmail.com>
2023-09-01 22:17:04 +0530

    Error fixed

--- app/myBookMarks/page.tsx
+++ app/myBookMarks/page.tsx
@@ -1,16 +1,9 @@
 import React from "react";
-import Card from "../components/Card";
 
 const MyBookMarks = () => {
   return (
     <div>
-      <div className=" md:my-20 grid gap-7 grid-cols-4">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-      </div>
+      <div className=" md:my-20 grid gap-7 grid-cols-4"></div>
     </div>
   );
 };
